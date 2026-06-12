bcaee1e22f610302480e2ea02aa99c5914e1ac29
Maureen Zoungrana <maureenzoungrana@gmail.com>
2026-06-12 20:21:41 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -103,4 +103,110 @@ document.querySelectorAll('#freelance .card').forEach(card => {
 });
 // ligne de copyright
 document.getElementById("copyright").innerHTML = "© " + new Date().getFullYear() + " AfriTalent" +" | Tous droits réservés";
+// barre de filtre
+function filtrerFreelances(categorie) {
+  // Gère l'état actif des boutons
+  const boutons = document.querySelectorAll('.filtre');
+  boutons.forEach(btn => btn.classList.remove('actif'));
+  event.target.classList.add('actif');
+
+  // Filtrage des cartes
+  const cartes = document.querySelectorAll('#freelance .card');
+  cartes.forEach(carte => {
+    const categorieCarte = carte.getAttribute('data-categorie');
+
+    if (categorie === 'tous' || categorieCarte === categorie) {
+      carte.classList.remove('masque'); // Affiche la carte
+      carte.classList.add('categorie', 'actif'); 
+    } else {
+      carte.classList.add('masque'); // Masque la carte
+    }
+  });
+}
+// validation du formulaire
+const formulaire = document.getElementById('formulaire');
+document.getElementById('formulaire').addEventListener('submit', function(event) {
+    event.preventDefault();     // Empêcher l'envoi du formulaire par défaut       
+    const nom = document.getElementById('nom');
+    const prenom = document.getElementById('prenom');
+    const email = document.getElementById('email');
+    const address = document.getElementById('address');
+    const telephone = document.getElementById('telephone');
+    const age = document.getElementById('age');
+    const message = document.getElementById('message');
+    // Réinitialisation des erreurs
+    resetErrors();
+    // Variables pour vérifier la validité
+    let isValid = true;
+    // Validation du Nom (minimum 2 caractères)
+    if (nom.value.trim().length < 2) {
+        showError('nomError', prenom.value.trim() === '' 
+            ? 'Ce champ est obligatoire.'  // si c'est vrai ecrire ...
+            : 'Le nom doit contenir au moins 2 caractères.');  // si c'est faux...
+        isValid = false;
+    }
+    // Validation du prénom (minimum 2 caractères)
+    if (prenom.value.trim().length < 2) {
+        showError('prenomError', prenom.value.trim() === '' 
+            ? 'Ce champ est obligatoire.' 
+            : 'Le prénom doit contenir au moins 2 caractères.'); 
+        isValid = false;
+    }
+    // Validation de l'Email (Regex standard)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.value.trim())) {
+        showError('emailError', 'Veuillez entrer une adresse email valide.');
+        isValid = false;
+    }
+    // Validation du numero de téléphone
+    const phoneRegex = /^(\+221\s?)?[0-9]{2}\s?\d{3}\s?\d{2}\s?\d{2}$/;
+    if (!phoneRegex.test(telephone.value.trim())) {
+      showError('phoneError', 'Veuillez entrer un numéro téléphone valide.');
+        isValid = false;
+    }
+    // validation de l'age
+    if (age.value.trim() == ''){
+      showError('ageError', 'Ce champ est obligatoire.');
+      isValid = false;
+    }
+    // Validation de l'addresse
+    if (address.value.trim() === '') {
+        showError('addressError', 'Ce champ est obligatoire.');
+        isValid = false;
+    }
+    // Validation du Message (non vide)
+    if (message.value.trim() === '') {
+        showError('messageError', 'Le message ne peut pas être vide.');
+        isValid = false;
+    }
+    // Soumission si tout est valide
+    // Soumission si tout est valide
+    if (isValid) {
+        // Récupérer les données du formulaire
+        const donneesFormulaire = new FormData(formulaire);
+        alert('Formulaire soumis avec succès !');
+        // Envoyer les données avec l'API Fetch
+        fetch('https://formspree.io/f/mqeodbkq', {
+            method: 'POST',
+            body: donneesFormulaire,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+    }
+});
+// Afficher les erreurs
+function showError(elementId, message) {
+    const errorElement = document.getElementById(elementId);
+    errorElement.textContent = message;
+    errorElement.parentElement.classList.add('invalid');
+}
+// Masquer les erreurs
+function resetErrors() {
+    const errorElements = document.querySelectorAll('.error');
+    errorElements.forEach(function(el) {
+        el.textContent = '';
+        el.parentElement.classList.remove('invalid');
+    });
+}
 
